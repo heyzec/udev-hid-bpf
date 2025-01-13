0c38a29f2cd46201621e07f6cabff6d31a281622
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2025-01-13 16:17:49 +0100

    tests: prevent segfaults when accessing struct hid_bpf_ctx fields
    `BPF_PROG` from `bpf_tracing.h` is a complex macro which turns the
    function into a `int fun(void* ctx)`, with the `struct hid_bpf_ctx` being
    hidden in an inlined inside with some bpf magic.
    
    For testing, we need to overwrite the macro and make it looks like
    a normal function, without the context. This is actually what our python
    wrapper expects.
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/libevdev/udev-hid-bpf/-/merge_requests/170>

--- src/bpf/uhid-bpf-test-wrappers.h
+++ src/bpf/uhid-bpf-test-wrappers.h
@@ -5,8 +5,17 @@
 #ifndef __UHID_BPF_TEST_WRAPPERS_H
 #define __UHID_BPF_TEST_WRAPPERS_H
 
+#include <bpf/bpf_tracing.h>
+
 #undef bpf_printk
 #include <stdio.h>
 #define bpf_printk(fmt, args...) printf(fmt "\n", ##args)
+
+/* BPF_PROG is a macro that creates the function with only
+ * a context as an argument, and deal with the extra parameters
+ * through some bpf magic. For testing, make it simple
+ */
+#undef BPF_PROG
+#define BPF_PROG(name, args...) name(args)
 
 #endif /* __UHID_BPF_TEST_WRAPPERS_H */
