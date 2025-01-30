heyzec/udev-hid-bpf

commit 328ba8857ee00e0008d41844c4181a4598406936

CI: replace make_release.sh with a Python version
Previously, make_install.sh would use rebuild udev-hid-bpf and use meson
introspect to generate an install.sh/uninstall.sh script that installed
all built artifacts.

We have a use-case for building the bpf.o files separately from the
binary so we can built the former with the latest libbpf but the latter
with the oldest glibc it works on for max. compatibility.

This commit adds two new scripts:
- make-release.py which replaces make_release.sh. Unlike the previous
  one this one does not actually rebuilt, instead it uses an installed
  tree and converts that over to a new target directory with the
  generated {un}install.sh files (using a jinja template)
- make-combined-release.sh: this replaces the logic previously in
  .gitlab-ci.yml so it's easier to test. It takes an installed
  tree, rebuilds udev-hid-bpf without any bpfs and then combines
  the installed tree and the rebuilt binary into the release tarball
  that we use as artifact of the gitlab CI job

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

.gitlab-ci/make-combined-release.sh
@@ -1,0 +1,59 @@
+#!/usr/bin/env bash
+#
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Script to help create a release tarball from two different
+# compiled versions of udev-hidp-bpf.
+#
+# The SRCDIR is an installed version of udev-hid-bpf. That install
+# is overwritten with just the rebuilt udev-hid-bpf (bpf.o files are left as-is)
+#
+
+set -e
+
+SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+
+usage() {
+  echo "Usage: $(basename "$0") SRCDIR"
+}
+
+SRCDIR="$1"
+
+if [[ -z "$SRCDIR" ]]; then
+  usage >&2
+  exit 1
+fi
+
+if ! [[ -e "$SRCDIR" ]]; then
+  echo "'$SRCDIR' does not exist" >&2
+  exit 1
+fi
+
+if ! [[ -e "$SRCDIR/bin/udev-hid-bpf" ]]; then
+  echo "Missing file $SRCDIR/bin/udev-hid-bpf" >&2
+  exit 1
+fi
+
+
+DSTDIR=$(mktemp -d _udev-hid-bpf-release-XXX)
+MESON_BUILDDIR=$(mktemp -d _builddir.XXXX)
+export MESON_BUILDDIR
+
+# Overwrite just udev-hid-bpf with one compat with this glibc
+export MESON_ARGS="--prefix=$SRCDIR -Dbpfs=[] -Dudevdir=$SRCDIR/lib/udev"
+
+"$SCRIPT_DIR"/meson-build.sh --skip-test --run-install
+
+python3 -m venv _venv
+# shellcheck disable=SC1091
+. _venv/bin/activate
+pip install jinja2
+
+"$SCRIPT_DIR"/make-release.py --verbose \
+   --template "$SCRIPT_DIR"/install.sh.jinja \
+   --template "$SCRIPT_DIR"/uninstall.sh.jinja \
+   "$SRCDIR" "$DSTDIR"
+
+TARBALL_NAME="udev-hid-bpf_$(git describe --tags)"
+tar cvaf "${TARBALL_NAME}.tar.xz" -C "$DSTDIR" --transform "s/^\./$TARBALL_NAME/" .
+echo "Tarball available as ${TARBALL_NAME}.tar.xz"

.gitlab-ci.yml
@@ -53,7 +53,8 @@ variables:
   FEDORA_TAG:  '2024-10-21.0'
   FDO_DISTRIBUTION_EXEC: 'pip3 install --break-system-packages git+https://gitlab.freedesktop.org/libevdev/hid-tools/'
   RUST_BACKTRACE: '1'
-  PREFIX: /_inst
+  INSTDIR: _inst
+  PREFIX: /$INSTDIR
   # Below is for our building of the kernel used in the b2c jobs. A separate tag
   # so we can rebuild that container kernel independently of the rest of the CI pipeline.
   KERNEL_BUILD_CONTAINER_TAG: '2024-04-10.3'
@@ -260,6 +261,7 @@ meson build and test:
   script:
     - .gitlab-ci/meson-build.sh --run-install --run-test
     - tree $PREFIX
+    - mv $PREFIX $INSTDIR
   variables:
     MESON_ARGS: --prefix=$PREFIX -Dbpfs=userhacks,stable,testing -Dudevdir=$PREFIX/lib/udev
   artifacts:
@@ -270,6 +272,7 @@ meson build and test:
       - $MESON_BUILDDIR/udev-hid-bpf*
       - $MESON_BUILDDIR/*.rules
       - $MESON_BUILDDIR/*.hwdb
+      - $INSTDIR
     when: on_success
     expire_in: 1 day
 
@@ -419,16 +422,11 @@ make release:
   script:
     # Fedora 37 can't properly use _Pragma under GCC
     - git apply .gitlab-ci/fedora-37.patch
-    - python3 -m venv _venv
-    - . _venv/bin/activate
-    - pip install yq
     - ln -s /usr/bin/true /usr/local/bin/udevadm
     - ln -s /usr/bin/true /usr/local/bin/systemd-hwdb
-    - VERSION=$(git describe --tags $DIRTY)
-    - TARBALL="udev-hid-bpf_${VERSION}.tar.xz"
-    - ./make_release.sh --verbose --features testing,stable --output "$TARBALL"
+    - .gitlab-ci/make-combined-release.sh $PWD/$INSTDIR
     - mkdir -p tarball_dir
-    - tar xf "$TARBALL" -C tarball_dir --strip-components=1
+    - tar xf udev-hid-bpf*.tar.xz -C tarball_dir --strip-components=1
     - pushd tarball_dir
     - ./install.sh --verbose $PREFIX
     - test -e $PREFIX/bin/udev-hid-bpf
@@ -442,8 +440,9 @@ make release:
     - test ! -e /lib/firmware/hid/bpf/0010-HP__Elite-Presenter.bpf.o
     - popd
     # We can't have variables/wildcards in artifacts:paths if we use expose_as
-    - cp $TARBALL udev-hid-bpf.tar.xz
+    - cp udev-hid-bpf*.tar.xz udev-hid-bpf.tar.xz
   variables:
+    MESON_BUILDDIR: build_dir
     FDO_DISTRIBUTION_VERSION: $FEDORA_RELEASE_VERSION
     PREFIX: /usr
     GIT_DEPTH: 0
@@ -453,6 +452,7 @@ make release:
       - udev-hid-bpf.tar.xz
   dependencies:
     - fedora-release@container-prep
+    - meson build and test
 
 
 doc-build:

.gitlab-ci/install.sh.jinja
@@ -57,9 +57,15 @@ find ./ -name "*.rules" -exec sed -i "s|@@BINDIR@@|$PREFIX/bin|" {} \;
 
 echo "Using sudo to install files into $PREFIX. You may be asked for your password now"
 dryrun_sudo="$DRY_RUN sudo"
-@@INSTALL_COMMANDS@@
-$dryrun_sudo install -D -t "$PREFIX"/bin/ "$SCRIPT_DIR"/bin/udev-hid-bpf
-$dryrun_sudo install -D -m 644 -t "$UDEVDIR"/udev/rules.d "$SCRIPT_DIR"/lib/udev/rules.d/81-hid-bpf.rules
-$dryrun_sudo install -D -m 644 -t "$UDEVDIR"/udev/hwdb.d "$SCRIPT_DIR"/lib/udev/hwdb.d/81-hid-bpf-*.hwdb
+{% for bpf in bpfs %}
+$dryrun_sudo install -D -t "$PREFIX/{{bpf.dir}}" "$SCRIPT_DIR/{{bpf.path}}"
+{% endfor %}
+{% for rule in udev_rules %}
+$dryrun_sudo install -D -m 644 -t "$UDEVDIR"/udev/rules.d "$SCRIPT_DIR"/{{rule.path}}
+{% endfor %}
+{% for hwdb in hwdb_files %}
+$dryrun_sudo install -D -m 644 -t "$UDEVDIR"/udev/hwdb.d "$SCRIPT_DIR"/{{hwdb.path}}
+{% endfor %}
+$dryrun_sudo install -D -t "$PREFIX/bin/" "$SCRIPT_DIR/bin/udev-hid-bpf"
 $dryrun_sudo udevadm control --reload
 $dryrun_sudo systemd-hwdb update

.gitlab-ci/make-release.py
@@ -1,0 +1,109 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Script to help create a release tarball for udev-hid-bpf.
+# Copies all items in INSTALL_TREE to the given target directory
+# and generates the install.sh/uninstall.sh files based on
+# the files in that tree.
+#
+# INSTALL_TREE must be a clean install tree with nothing but the udev-hid-bpf
+# files.
+
+from dataclasses import dataclass
+from pathlib import Path
+
+import argparse
+import jinja2
+import jinja2.environment
+import shutil
+import logging
+
+
+logger = logging.getLogger("")
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)s: %(message)s")
+logger.setLevel(logging.INFO)
+
+parser = argparse.ArgumentParser(
+    description="Creates a release tree from an installed udev-hid-bpf directory"
+)
+parser.add_argument("--verbose", action="store_true", default=False)
+parser.add_argument(
+    "--templates",
+    action="append",
+    type=Path,
+    help="A jinja template to parse and place in the top-level directory of the TARGET_DIR",
+)
+parser.add_argument(
+    "source_dir",
+    metavar="INSTALL_TREE",
+    type=Path,
+    help="A meson install-ed tree of udev-hid-bpf",
+)
+parser.add_argument(
+    "target_dir",
+    metavar="TARGET_DIR",
+    type=Path,
+    help="Target directory to place all files",
+)
+args = parser.parse_args()
+
+if args.verbose:
+    logger.setLevel(logging.DEBUG)
+
+
+@dataclass
+class InstallFile:
+    path: Path
+
+    @property
+    def dir(self) -> Path:
+        return self.path.parent
+
+    @property
+    def filename(self) -> str:
+        return self.path.name
+
+
+bpfs = []
+udev_rules = []
+hwdb_files = []
+# First copy our installed tree over
+for file in args.source_dir.glob("**/*"):
+    if file.is_dir():
+        continue
+
+    path = file.relative_to(args.source_dir)
+    target = args.target_dir / path
+    target.parent.mkdir(exist_ok=True, parents=True)
+    logger.debug(f"{target}")
+    shutil.copyfile(file, target)
+    shutil.copymode(file, target)
+
+    ifile = InstallFile(target.relative_to(args.target_dir))
+
+    if target.name.endswith(".bpf.o"):
+        bpfs.append(ifile)
+    elif target.name.endswith(".rules"):
+        udev_rules.append(ifile)
+    elif target.name.endswith(".hwdb"):
+        hwdb_files.append(ifile)
+
+
+data = {"bpfs": bpfs, "udev_rules": udev_rules, "hwdb_files": hwdb_files}
+
+for template in args.templates:
+    template_dir = template.absolute().parent
+    loader = jinja2.FileSystemLoader(template_dir)
+    env = jinja2.Environment(
+        loader=loader,
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+    jtemplate = env.get_template(template.name)
+    stream = jtemplate.stream(data)
+    target = args.target_dir / template.name.rstrip(".jinja")
+    logger.debug(target)
+    with open(target, "w") as fd:
+        stream.dump(fd)
+    shutil.copymode(template, target)

.gitlab-ci/uninstall.sh.jinja
@@ -55,9 +55,15 @@ fi
 
 echo "Using sudo to remove files from $PREFIX. You may be asked for your password now"
 dryrun_sudo="$DRY_RUN sudo"
-@@UNINSTALL_COMMANDS@@
+{% for bpf in bpfs %}
+$dryrun_sudo rm -f "$PREFIX/{{bpf.path}}"
+{% endfor %}
+{% for udev in udev_rules %}
+$dryrun_sudo rm -f "$UDEVDIR"/udev/rules.d/{{udev.filename}}
+{% endfor %}
+{% for hwdb in hwdb_files %}
+$dryrun_sudo rm -f "$UDEVDIR"/udev/hwdb.d/{{hwdb.filename}}
+{% endfor %}
 $dryrun_sudo rm -f "$PREFIX"/bin/udev-hid-bpf
-$dryrun_sudo rm -f "$UDEVDIR"/udev/rules.d/81-hid-bpf.rules
-$dryrun_sudo rm -f "$UDEVDIR"/udev/hwdb.d/81-hid-bpf-*.hwdb
 $DRY_RUN sudo udevadm control --reload
 $DRY_RUN sudo systemd-hwdb update

make_release.sh
@@ -1,96 +1,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
-
-usage () {
-  echo "Usage: $(basename "$0") [-v|--verbose]"
-}
-
-DIRTY="--dirty"
-
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-    --help)
-      usage
-      exit 0
-      ;;
-    --verbose|-v)
-      set -x
-      shift
-      ;;
-    --yes-I-really-do-not-want-dirty-suffix)
-      DIRTY=""
-      shift
-      ;;
-    --features)
-      features=$2
-      shift 2
-      ;;
-    --output)
-      TARNAME=$2
-      shift 2
-      ;;
-    --*)
-      usage
-      exit 1
-      ;;
-    *)
-      usage
-      exit 1
-      ;;
-  esac
-done
-
-if [ $# -gt 1 ]; then
-  usage
-  exit 1
-fi
-
-if [[ -n "$TARNAME" ]]; then
-  if ! [[  "$TARNAME" = *".tar.xz" ]]; then
-    echo "--output must have suffix .tar.xz"
-    exit 1
-  fi
-  TARNAME=${TARNAME/.tar.xz}
-fi
-
-
-set -e
-
-if [ -n "$features" ]; then
-  features="-Dbpfs=$features"
-fi
-
-TMP_INSTALL_DIR=$PWD/_inst
-BUILDDIR=_release_builddir
-meson setup \
-  -Dprefix="$TMP_INSTALL_DIR" \
-  -Dudevdir="$TMP_INSTALL_DIR/lib/udev" \
-  -Dplaceholder-udev-rules-file=true \
-  $features \
-  "$BUILDDIR"
-meson compile -C "$BUILDDIR"
-meson install -C "$BUILDDIR"
-
-if [[ -z "$TARNAME" ]]; then
-  VERSION=$(git describe --tags $DIRTY)
-  TARNAME=udev-hid-bpf_${VERSION}
-fi
-
-# Now get the list of installed files and generate install.sh and uninstall.sh with it
-
-installed=$(meson introspect _release_builddir --installed | yq -r ".[] | sub(\"$TMP_INSTALL_DIR/\"; \"\")")
-INSTALL_COMMANDS=""
-UNINSTALL_COMMANDS=""
-for f in $installed; do
-  if [[ "$f" == *bpf.o* ]]; then
-    INSTALL_COMMANDS="$INSTALL_COMMANDS\n\$dryrun_sudo install -D -t \"\$PREFIX/$(dirname "$f")\" \"\$SCRIPT_DIR/$f\""
-    UNINSTALL_COMMANDS="$UNINSTALL_COMMANDS\n\$dryrun_sudo rm -f \"\$PREFIX/$f\""
-  fi
-done
-
-sed -e "s|@@INSTALL_COMMANDS@@|$INSTALL_COMMANDS|" release_install.sh.in > "$TMP_INSTALL_DIR"/install.sh
-sed -e "s|@@UNINSTALL_COMMANDS@@|$UNINSTALL_COMMANDS|" release_uninstall.sh.in > "$TMP_INSTALL_DIR"/uninstall.sh
-chmod +x "$TMP_INSTALL_DIR"/install.sh "$TMP_INSTALL_DIR"/uninstall.sh
-
-tar cvaf ${TARNAME}.tar.xz -C "$TMP_INSTALL_DIR" --transform "s/^\./$TARNAME/" .

commit 39a1f48b7e285ee14cedd22c40190999878c6bf3

CI: don't replace @@BINDIR@@ in the udev rules for releases
Fixes: 328ba8857ee0 ("CI: replace make_release.sh with a Python version")

Closes #50

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
Part-of: <https://gitlab.freedesktop.org/libevdev/udev-hid-bpf/-/merge_requests/176>

.gitlab-ci/make-combined-release.sh
@@ -40,7 +40,7 @@ MESON_BUILDDIR=$(mktemp -d _builddir.XXXX)
 export MESON_BUILDDIR
 
 # Overwrite just udev-hid-bpf with one compat with this glibc
-export MESON_ARGS="--prefix=$SRCDIR -Dbpfs=[] -Dudevdir=$SRCDIR/lib/udev"
+export MESON_ARGS="--prefix=$SRCDIR -Dbpfs=[] -Dudevdir=$SRCDIR/lib/udev -Dplaceholder-udev-rules-file=true"
 
 "$SCRIPT_DIR"/meson-build.sh --skip-test --run-install
 