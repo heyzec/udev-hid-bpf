heyzec/udev-hid-bpf

commit 3c6d84f4d078f6373db06a5fbce1c3e8f0357586

hidudev: move the ignore property check into a helper
And call that helper from cmd_add(). This slightly changes the
functionality: previously HID_IGNORE_DEVICE was only honoured for the
our lookup of matching BPF files but not for loading BPFs provided
on the commandline.

That behavior may be useful for some debugging but realistically it's
better to expect the caller removes that udev property so we have
predictable behavior.

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

src/main.rs
@@ -165,11 +165,13 @@ fn cmd_add(
 
     for syspath in devices {
         let dev = hidudev::HidUdev::from_syspath(syspath)?;
-        if objfiles.is_empty() {
-            dev.load_bpf_from_directories(&target_bpf_dirs, None, properties)?;
-        } else {
-            for objfile in objfiles {
-                dev.load_bpf_from_directories(&target_bpf_dirs, Some(objfile), properties)?;
+        if !dev.is_ignored() {
+            if objfiles.is_empty() {
+                dev.load_bpf_from_directories(&target_bpf_dirs, None, properties)?;
+            } else {
+                for objfile in objfiles {
+                    dev.load_bpf_from_directories(&target_bpf_dirs, Some(objfile), properties)?;
+                }
             }
         }
     }

src/hidudev.rs
@@ -94,6 +94,12 @@ impl HidUdev {
             .collect()
     }
 
+    pub fn is_ignored(&self) -> bool {
+        self.udev_device
+            .property_value("HID_BPF_IGNORE_DEVICE")
+            .is_some()
+    }
+
     /// Find the given file name in the set of directories, returning a path
     /// to the first filename found. The directories are assumed in preference
     /// order, first match wins.
@@ -200,13 +206,6 @@ impl HidUdev {
             None => {
                 if !bpf_dirs.iter().any(|d| d.exists()) {
                     log::warn!("bpf directories {:?} don't exist, aborting", bpf_dirs);
-                    return Ok(());
-                }
-                if self
-                    .udev_device
-                    .property_value("HID_BPF_IGNORE_DEVICE")
-                    .is_some()
-                {
                     return Ok(());
                 }
                 self.search_for_matching_objfiles(bpf_dirs)

commit 0c4a275e41c6415394b2c0cfab15532734d21712

hidudev: re-enable force loading of bpf objects when a path is given
Regression introduced in 3c6d84f ("hidudev: move the ignore property
check into a helper").

When we call `udev-hid-bpf` from the command line with a bpf path as an
argument, it doesn't make sense to check for the `HID_BPF_IGNORE_DEVICE`
property. The user actually wants the file to be added, and the property
is here to prevent any system BPF file to be loaded.

Closes: #67
Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>
Part-of: <https://gitlab.freedesktop.org/libevdev/udev-hid-bpf/-/merge_requests/199>

src/main.rs
@@ -188,20 +188,20 @@ fn cmd_add(
 
     for syspath in devices {
         let dev = hidudev::HidUdev::from_syspath(syspath)?;
-        if !dev.is_ignored() {
-            if objfiles.is_empty() {
+        if objfiles.is_empty() {
+            if !dev.is_ignored() {
                 let objfiles = dev.search_for_matching_objfiles(&target_bpf_dirs);
                 dev.load_bpf_files(&objfiles, properties)?;
             } else {
-                let bpf_files = hidudev::HidUdev::find_named_objfiles(&objfiles, &target_bpf_dirs);
-                if bpf_files.is_empty() {
-                    log::warn!("Unable to find any BPF programs for: {:?}", objfiles);
-                } else {
-                    dev.load_bpf_files(&bpf_files, properties)?;
-                }
+                log::warn!("Device {syspath:?} has HID_BPF_IGNORE_DEVICE set, skipping");
             }
         } else {
-            log::warn!("Device {syspath:?} has HID_BPF_IGNORE_DEVICE set, skipping");
+            let bpf_files = hidudev::HidUdev::find_named_objfiles(&objfiles, &target_bpf_dirs);
+            if bpf_files.is_empty() {
+                log::warn!("Unable to find any BPF programs for: {:?}", objfiles);
+            } else {
+                dev.load_bpf_files(&bpf_files, properties)?;
+            }
         }
     }
 